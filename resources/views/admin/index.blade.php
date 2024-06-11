bd83d40dbba5eb2c8e48b4b03f67193375fffae0
devFarjanur <farjanur15-2956@diu.edu.bd>
2024-06-11 15:28:15 +0600

    Question Bank

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -62,20 +62,6 @@
             </div>
           </div>
         </div>
-        <div class="col-md-3 grid-margin stretch-card">
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex justify-content-between align-items-baseline">
-                <h6 class="card-title mb-0">Fee Collection</h6>
-              </div>
-              <div class="row">
-                <div class="col-6 col-md-12 col-xl-5">
-                  <h3 class="mb-2"></h3>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -104,7 +90,7 @@
       </div>
       </a>
     </div>
-  @endforeach
+  @endforeach 
   </div>
 
 
