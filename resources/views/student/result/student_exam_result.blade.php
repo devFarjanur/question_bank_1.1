f42d0f02a05293b1f36dfcbd68de015ee2327da1
devFarjanur <farjanur15-2956@diu.edu.bd>
2024-06-29 02:13:16 +0600

    Question Bank

--- resources/views/student/result/student_exam_result.blade.php
+++ resources/views/student/result/student_exam_result.blade.php
@@ -9,41 +9,6 @@
         </ol>
     </nav>
 
-    <!-- MCQ Exam Scores -->
-    <div class="card">
-        <div class="card-header">
-            <h4>MCQ Exam Scores</h4>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th class="text-center">Exam Name</th>
-                            <th class="text-center">Question Chapter</th>
-                            <th class="text-center">Question Category</th>
-                            <th class="text-center">Total Score</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($mcqScores as $examName => $chapters)
-                            @foreach($chapters as $chapterName => $categories)
-                                @foreach($categories as $categoryName => $totalScore)
-                                    <tr>
-                                        <td class="text-center">{{ $examName }}</td>
-                                        <td class="text-center">{{ $chapterName }}</td>
-                                        <td class="text-center">{{ $categoryName }}</td>
-                                        <td class="text-center">{{ $totalScore }}</td>
-                                    </tr>
-                                @endforeach
-                            @endforeach
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-
     <!-- Blooms Exam Scores -->
     <div class="card mt-4">
         <div class="card-header">
@@ -88,6 +53,43 @@
             </div>
         </div>
     </div>
+
+    <!-- MCQ Exam Scores -->
+    <div class="card">
+        <div class="card-header">
+            <h4>MCQ Exam Scores</h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th class="text-center">Exam Name</th>
+                            <th class="text-center">Question Chapter</th>
+                            <th class="text-center">Question Category</th>
+                            <th class="text-center">Total Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($mcqScores as $examName => $chapters)
+                            @foreach($chapters as $chapterName => $categories)
+                                @foreach($categories as $categoryName => $totalScore)
+                                    <tr>
+                                        <td class="text-center">{{ $examName }}</td>
+                                        <td class="text-center">{{ $chapterName }}</td>
+                                        <td class="text-center">{{ $categoryName }}</td>
+                                        <td class="text-center">{{ $totalScore }}</td>
+                                    </tr>
+                                @endforeach
+                            @endforeach
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
 
 </div>
 
